149cadb7cabf58f6de4807800b891602232127f1
bulainarciss <bulainarciss@github.com>
2020-12-26 13:00:48 +0200

    Little Turtle Drawings

---
+++ main.py
@@ -0,0 +1,47 @@
+from turtle import *
+import time
+
+pencolor('purple')
+bgcolor('black')
+penup()
+goto(0, 200)
+pendown()
+speed(0)
+hideturtle()
+time.sleep(5)
+while True:
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('yellow')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    pencolor('white')
+    forward(20)
+    right(10)
+    right(120)
+
+    if abs(pos()) < 1:
+        break
+
+
+
+done()
